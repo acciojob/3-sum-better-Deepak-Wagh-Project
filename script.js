5280a9194fdb357bf318f9fae82b1e21666bc68f
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-12-03 22:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function threeSum(arr, target) {
+	let min=Infinity;
+	let nearestSum=0;
+	for(let i=0;i<arr.length-2;i++){
+		for(let j=i;j<arr.length-1;j++){
+			for(let k=j;k<arr.length;k++)
+				let sum=arr[i]+arr[j]+arr[k];
+			   if(Math.abs(target-sum)<min){ 
+				   min=Math.abs(target-sum);
+				   nearestSum=sum;
+}
+		}
+		return nearestSum;
+	}
 // write your code here
   
 }
